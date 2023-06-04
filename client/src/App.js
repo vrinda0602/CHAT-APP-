0c9aeec053d9eeb3567c87dad8f138e1d87f872d
Vrinda Maheshwari <84084549+vrinda0602@users.noreply.github.com>
2023-06-05 01:24:05 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
           <h3>Join A Chat</h3>
           <input
             type="text"
-            placeholder="John..."
+            placeholder="Name..."
             onChange={(event) => {
               setUsername(event.target.value);
             }}
